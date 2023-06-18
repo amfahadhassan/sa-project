54bdbd6a0df45f23b39b16f348c6b293f0b2fb1f
Tushar Ghosh <tusharghosh09006@gmail.com>
2023-06-18 16:21:40 -0500

    add log service

---
+++ logService/src/main/java/edu/miu/logService/LogServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.logService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogServiceApplication.class, args);
+	}
+
+}
