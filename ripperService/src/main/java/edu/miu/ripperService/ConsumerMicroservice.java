f9904d07581d0bf58dbc99fbcdb66771a4c2f965
bindum31 <bindum31@gmail.com>
2023-06-19 17:15:33 -0500

    first commit
    first commit

---
+++ ripperService/src/main/java/edu/miu/ripperService/ConsumerMicroservice.java
@@ -0,0 +1,22 @@
+package edu.miu.ripperService;
+
+import java.time.LocalDateTime;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConsumerMicroservice {
+	
+	
+	
+	
+
+	@KafkaListener(topics = {"earthquake"})
+	public void receive(@Payload String message) {
+	    System.out.println("Receiver received message= "+ message);
+	    //EarthQuake earthQuake1 = new EarthQuake("ak0237teb8pn", 2.7, "CA", LocalDateTime.now());
+	    //earthQuakeRepository.save(earthQuake1);
+	}
+}
--- ripperService/src/main/java/edu/miu/ripperService/RipperServiceApplication.java
+++ ripperService/src/main/java/edu/miu/ripperService/RipperServiceApplication.java
@@ -2,12 +2,15 @@ package edu.miu.ripperService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class RipperServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RipperServiceApplication.class, args);
+		System.out.println("========Consumer+Ripper========");
 	}
 
 }
